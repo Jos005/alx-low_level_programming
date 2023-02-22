8ac6ad83714f005b83cfe246d8634534bfd7fec2
Jos005 <ayamjoseph@gmail.com>
2023-02-22 08:20:21 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,15 @@
 /**
  * print_alphabet - print alphabets in lower case
  *
+ * Return: 0
  */
 void print_alphabet(void)
 {
-	char letter;
+	char c;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
-         _putchar('\n')
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+        _putchar('\n')
 }
